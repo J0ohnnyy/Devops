fbe53e29b80517d0746caa5b7eae6de784bd7c9e
Karim <93717925+J0ohnnyy@users.noreply.github.com>
2024-03-01 15:23:37 +0100

    Calcule script

---
+++ demo/src/main/java/com/example/demo/Calcul.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class Calcul{
+
+    public int add(int a , int b) {
+
+        return  a+b;
+    }
+
+    public int multi(int a , int b) {
+
+        return  a*b;
+    }
+
+    public int div(int a , int b) {
+
+        return a/b;
+    }
+} 
